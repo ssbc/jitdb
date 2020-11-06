3202592488d016bdf9f41ce2b0588b31f20b00b5
Anders Rune Jensen <arj03@protonmail.ch>
2020-11-06 11:18:20 +0100

    faster helpers

--- helpers.js
+++ helpers.js
@@ -1,55 +1,64 @@
 const bipf = require('bipf')
 
+const bValue = Buffer.from('value')
+const bAuthor = Buffer.from('author')
+const bContent = Buffer.from('content')
+const bType = Buffer.from('type')
+const bRoot = Buffer.from('root')
+const bMeta = Buffer.from('meta')
+const bPrivate = Buffer.from('private')
+const bChannel = Buffer.from('channel')
+
 module.exports = {
   seekAuthor: function(buffer) {
     var p = 0 // note you pass in p!
-    p = bipf.seekKey(buffer, p, 'value')
+    p = bipf.seekKey(buffer, p, bValue)
 
     if (~p)
-      return bipf.seekKey(buffer, p, 'author')
+      return bipf.seekKey(buffer, p, bAuthor)
   },
 
   seekType: function(buffer) {
     var p = 0 // note you pass in p!
-    p = bipf.seekKey(buffer, p, 'value')
+    p = bipf.seekKey(buffer, p, bValue)
 
     if (~p) {
-      p = bipf.seekKey(buffer, p, 'content')
+      p = bipf.seekKey(buffer, p, bContent)
       if (~p)
-        return bipf.seekKey(buffer, p, 'type')
+        return bipf.seekKey(buffer, p, bType)
     }
   },
 
   seekRoot: function(buffer) {
     var p = 0 // note you pass in p!
-    p = bipf.seekKey(buffer, p, 'value')
+    p = bipf.seekKey(buffer, p, bValue)
 
     if (~p) {
-      p = bipf.seekKey(buffer, p, 'content')
+      p = bipf.seekKey(buffer, p, bContent)
       if (~p)
-        return bipf.seekKey(buffer, p, 'root')
+        return bipf.seekKey(buffer, p, bRoot)
     }
   },
 
   seekPrivate: function(buffer) {
     var p = 0 // note you pass in p!
-    p = bipf.seekKey(buffer, p, 'value')
+    p = bipf.seekKey(buffer, p, bValue)
 
     if (~p) {
-      p = bipf.seekKey(buffer, p, 'meta')
+      p = bipf.seekKey(buffer, p, bMeta)
       if (~p)
-        return bipf.seekKey(buffer, p, 'private')
+        return bipf.seekKey(buffer, p, bPrivate)
     }
   },
 
   seekChannel: function(buffer) {
     var p = 0 // note you pass in p!
-    p = bipf.seekKey(buffer, p, 'value')
+    p = bipf.seekKey(buffer, p, bValue)
 
     if (~p) {
-      p = bipf.seekKey(buffer, p, 'content')
+      p = bipf.seekKey(buffer, p, bContent)
       if (~p)
-        return bipf.seekKey(buffer, p, 'channel')
+        return bipf.seekKey(buffer, p, bChannel)
     }
   }
 }
