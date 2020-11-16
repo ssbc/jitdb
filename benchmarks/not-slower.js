1dea2a5dbedb6b99a8677595c2bff351cb4694a6
Andre Staltz <andre@staltz.com>
2020-11-16 13:16:25 +0200

    experimenting

--- benchmarks/not-slower.js
+++ benchmarks/not-slower.js
@@ -3,28 +3,34 @@ const path = require('path')
 const rimraf = require('rimraf')
 const FlumeLog = require('async-flumelog')
 const JITDB = require('../index')
+const cpuStat = require('cpu-stat')
 const {query, fromDB, and, or, slowEqual, toCallback } = require('../operators')
 
 const fixture = path.join(__dirname, '..', 'fixture')
 const logPath = path.join(fixture, 'flume', 'log.bipf')
 const indexesDir = path.join(fixture, 'indexes')
 
-const alice = '@Sz83Igu5dM9u2b49SGXmaLq5mRudVIFFawReZYSnQuQ=.ed25519'
-const bob = '@WZdJI0tM9IxsIcqDRMnfhEUQtg0uiWb6ojZh1fjFU1Q=.ed25519'
+const alice = "@58u/J9+5bOXeYRDCYQ9cJ7kklghIpQFPBYxlhKq1/qs=.ed25519"
+const bob = "@0/c5XBu91ciAa+PjmXI5QXkOGPx+iEXO2Q2OKmdne6E=.ed25519"
 
-function compare(duration, max) {
-  return '' + duration + 'ms is less than ' + max + 'ms'
+const avgClockMHz = cpuStat.avgClockMHz()
+
+function compare(val, max) {
+  return '' + val + ' is less than ' + max
 }
 
 test('install and onReady', (t) => {
+  t.pass('CPU clock is on average ' + avgClockMHz + ' MHZ')
   const start = Date.now()
   const raf = FlumeLog(logPath, { blockSize: 64*1024 })
   rimraf.sync(indexesDir)
   const db = JITDB(raf, indexesDir)
   db.onReady(() => {
     const duration = Date.now() - start
-    const max = 15
-    t.true(duration < max, compare(duration, max))
+    const durationPerMHz = duration/avgClockMHz
+    console.log(duration, durationPerMHz)
+    const max = 0.004
+    t.true(durationPerMHz < max, compare(durationPerMHz, max))
     t.end()
   })
 })
@@ -41,9 +47,11 @@ test('one index (first run)', (t) => {
       toCallback((err, msgs) => {
         t.error(err)
         const duration = Date.now() - start
-        const max = 11000
-        t.true(duration < max, compare(duration, max))
-        t.equals(msgs.length, 235587)
+        const durationPerMHz = duration/avgClockMHz
+        console.log(duration, durationPerMHz)
+        const max = 0.25
+        t.true(durationPerMHz < max, compare(durationPerMHz, max))
+        t.equals(msgs.length, 23310)
         t.end()
       })
     )
@@ -61,9 +69,11 @@ test('one index (second run)', (t) => {
       toCallback((err, msgs) => {
         t.error(err)
         const duration = Date.now() - start
-        const max = 4400
-        t.true(duration < max, compare(duration, max))
-        t.equals(msgs.length, 235587)
+        const durationPerMHz = duration/avgClockMHz
+        console.log(duration, durationPerMHz)
+        const max = 0.11
+        t.true(durationPerMHz < max, compare(durationPerMHz, max))
+        t.equals(msgs.length, 23310)
         t.end()
       })
     )
@@ -87,9 +97,11 @@ test('many indexes (first run)', (t) => {
       toCallback((err, msgs) => {
         t.error(err)
         const duration = Date.now() - start
-        const max = 20000
-        t.true(duration < max, compare(duration, max))
-        t.equals(msgs.length, 471481)
+        const durationPerMHz = duration/avgClockMHz
+        console.log(duration, durationPerMHz)
+        const max = 0.4
+        t.true(durationPerMHz < max, compare(durationPerMHz, max))
+        t.equals(msgs.length, 47889)
         t.end()
       })
     )
@@ -112,9 +124,11 @@ test('many indexes (second run)', (t) => {
       toCallback((err, msgs) => {
         t.error(err)
         const duration = Date.now() - start
-        const max = 7000
-        t.true(duration < max, compare(duration, max))
-        t.equals(msgs.length, 471481)
+        const durationPerMHz = duration/avgClockMHz
+        console.log(duration, durationPerMHz)
+        const max = 0.17
+        t.true(durationPerMHz < max, compare(durationPerMHz, max))
+        t.equals(msgs.length, 47889)
         t.end()
       })
     )
--- benchmarks/setup-fixture.sh
+++ benchmarks/setup-fixture.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # wget -c \
-#   https://github.com/ssb-ngi-pointer/ssb-fixtures/releases/download/2.2.0/v2-sloop-m100000-a2000.tar.gz \
-#   -O - | tar -xz --one-top-level=sloop
+#   https://github.com/ssb-ngi-pointer/ssb-fixtures/releases/download/2.2.0/v2-catamaran-m1000000-a8000.tar.gz \
+#   -O - | tar -xz --one-top-level=fixture
 
 wget -c \
-  https://github.com/ssb-ngi-pointer/ssb-fixtures/releases/download/2.2.0/v2-catamaran-m1000000-a8000.tar.gz \
+  https://github.com/ssb-ngi-pointer/ssb-fixtures/releases/download/2.2.0/v2-sloop-m100000-a2000.tar.gz \
   -O - | tar -xz --one-top-level=fixture
 
 touch fixture/flume/log.bipf
